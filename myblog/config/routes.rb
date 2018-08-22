b9e5f4eeadf35ea774c5827e45445b6d74a56fbd
tatumnmix <tatumnmix@gmail.com>
2018-08-22 00:59:43 +0000

    余計なroutingを除外-2(pathに間違いがあったので修正)

--- myblog/config/routes.rb
+++ myblog/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     resources :posts do
-      post 'posts/:post_id/comments', to: 'comments#create', as: 'comments'
-      delete 'posts/:post_id/comments/:id', to: 'comments#destroy', as: 'comment'
+      post 'comments', to: 'comments#create', as: 'comments'
+      delete 'comments/:id', to: 'comments#destroy', as: 'comment'
     end
     root 'posts#index'
 end
