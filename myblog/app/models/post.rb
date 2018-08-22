6c8edb2d6b578c6c54a945017be5c3dde039b8f5
tatumnmix <tatumnmix@gmail.com>
2018-08-22 02:03:12 +0000

    画像投稿機能を追加-1

--- myblog/app/models/post.rb
+++ myblog/app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+    mount_uploader :picture, PictureUploader
     has_many :comments, dependent: :destroy
     validates:title,presence:true,length:{minimum: 3 ,message: 'Too short to post!'}
     validates:body,presence:true
--- myblog/db/schema.rb
+++ myblog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_21_050615) do
+ActiveRecord::Schema.define(version: 2018_08_22_012629) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_08_21_050615) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
 end
